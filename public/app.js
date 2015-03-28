2df1e0501685beae606316dd98f0428431efe4c4
Paweł Waleczek <pawel@thisismyasterisk.org>
2015-03-28 21:19:36 +0100

    update to knapff

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@
 
 /* globals cq */
 
-(function main() {
+// (function main() {
   var canvas = null
     , buffer = null
     , mousePos = {}
@@ -27,27 +27,156 @@
         , randomSpacing: false
         , moving: true
       }
+    , flies = []
     , audioContext
-    , analyser
+    , audioAnalyser
+    , zMod = false
+    , bgImage = new Image()
+
+  function extend(obj) {
+    [].forEach.call([].slice.call(arguments, 1), function(source) {
+      if (source) {
+        for (var prop in source) {
+          obj[prop] = source[prop]
+        }
+      }
+    })
+    return obj
+  }
+
+  function constrain(val, min, max) {
+    if (val > max) val = max
+    else if (val < min) val = min
+
+    return val
+  }
+
+  // Fly
+
+  function Fly(options) {
+    this.rangeMax = 10
+    this.range = 4
+    this.speedMax = 6
+    this.speed = 2
+    // this.position = extend(((options && ptions.position) || {}), position)
+    this.target = null
+    this.dir = {
+        x: 1
+      , y: 1
+    }
+    this.steps = 0
+    this.maxZoom = 15
+    this.targetRange = 75
+    this.zMod = 1
+    this.position = {
+        x: Math.random() * (canvas.width)
+      , y: Math.random() * (canvas.height)
+      , z: Math.random() * (this.maxZoom)
+    }
+    console.log(this.position)
+    this.setTarget()
+
+    return this
+  }
+
+  Fly.prototype.move = function move() {
+    var x = this.position.x
+      , y = this.position.y
+      , z = this.position.z
+      , dir = this.dir
+
+    if (Math.random() > 0.5) {
+      this.speed = constrain(Math.random() * this.speedMax, 5, this.speedMax)
+    }
+
+    x += this.zMod * this.steps * this.target.xDist * ( Math.random() * this.range)
+    y += this.zMod * this.steps * this.target.yDist * ( Math.random() * this.range)
+    z += this.zMod * this.steps * this.target.zDist * ( Math.random() * this.range)
+
+    this.position.x = constrain(x, 0 - this.position.z, canvas.width - this.position.z)
+    this.position.y = constrain(y, 0 - this.position.z, canvas.height - this.position.z)
+    this.position.z = constrain(z, 0, this.maxZoom)
+  }
+
+  Fly.prototype.setTarget = function setTarget() {
+    var x = Math.random() * canvas.width
+      , y = Math.random() * canvas.height
+      , z = Math.random() * canvas.height
+      , xDist = x - this.position.x
+      , yDist = y - this.position.y
+      , zDist = z - this.position.z
+
+    this.target = {
+        x: x
+      , y: y
+      , z: Math.random() > 0.7 ? z : this.position.z
+      , xDist: xDist
+      , yDist: yDist
+      , zDist: zDist
+    }
+
+    this.steps = this.speed / Math.abs(Math.sqrt(xDist * xDist + yDist * yDist))
+    console.log( this.speed, Math.abs(Math.sqrt(xDist * xDist + yDist * yDist)))
+    console.log('target ----> %d, %d, %f, %f, steps -> %f', this.target.x, this.target.y,this.target.xDist, this.target.yDist, this.steps)
+    console.log(this.zMod)
+  }
+
+  Fly.prototype.calculateZMod = function calculateZMod(zMod) {
+    if (window.zMod == true)
+      this.zMod += (zMod - this.zMod)
+    else
+      this.zMod = 1
+  }
+
+  Fly.prototype.checkTarget = function checkTarget() {
+    var x = this.position.x
+      , y = this.position.y
+      , z = this.position.z
+      , tx = this.target.x
+      , ty = this.target.y
+      , tz = this.target.z
+
+    return (
+         Math.abs(tx - x) < this.targetRange
+      && Math.abs(ty - y) < this.targetRange
+    )
+  }
+
+  Fly.prototype.draw = function draw(buffer) {
+    buffer.beginPath()
+    buffer.arc(
+        this.position.x
+      , this.position.y
+      , this.position.z // level 0 is 30px radius
+      , 0
+      , 2 * Math.PI
+      , false
+    )
+    buffer.closePath()
+    buffer.fillStyle('#000')
+    buffer.fill()
+  }
+
+  // end Fly
 
   function run(ev) {
-    var gui = new dat.GUI()
+    // var gui = new dat.GUI()
 
-    gui.add(options, 'flicker', -10, 10)
-    gui.add(options, 'elGridX', 1, 9)
-    gui.add(options, 'elGridY', 1, 9)
-    gui.add(options, 'elGridSpacing', 20, 500)
-    gui.add(options.x, 'fn', ['sin', 'cos', 'tan'])
-    gui.add(options.x, 'mx', 0, 100)
-    gui.add(options.y, 'fn', ['sin', 'cos', 'tan'])
-    gui.add(options.y, 'mx', 0, 100)
-    gui.add(options, 'pause')
-    gui.add(options, 'moving')
-    gui.add(options, 'randomSpacing')
+    // gui.add(options, 'flicker', -10, 10)
+    // gui.add(options, 'elGridX', 1, 9)
+    // gui.add(options, 'elGridY', 1, 9)
+    // gui.add(options, 'elGridSpacing', 20, 500)
+    // gui.add(options.x, 'fn', ['sin', 'cos', 'tan', 'log', 'exp', 'atan'])
+    // gui.add(options.x, 'mx', 0, 100)
+    // gui.add(options.y, 'fn', ['sin', 'cos', 'tan', 'log', 'exp', 'atan'])
+    // gui.add(options.y, 'mx', 0, 100)
+    // gui.add(options, 'pause')
+    // gui.add(options, 'moving')
+    // gui.add(options, 'randomSpacing')
 
     canvas = cq().appendTo(document.body)
     buffer = cq()
-
+    bgImage.src = '/bg.png'
     audioContext = new AudioContext()
 
     navigator.webkitGetUserMedia({
@@ -63,9 +192,13 @@
     }, function(stream) {
       mediaStreamSource = audioContext.createMediaStreamSource(stream)
 
-      analyser = audioContext.createAnalyser()
-      analyser.fftSize = 2048
-      mediaStreamSource.connect( analyser )
+      audioAnalyser = audioContext.createAnalyser()
+      audioAnalyser.fftSize = 2048
+      mediaStreamSource.connect(audioAnalyser)
+
+      for (var i = 0; i < 400; i++) {
+        flies.push(new Fly())
+      }
     }, function() { console.log('error', arguments) })
 
 
@@ -95,44 +228,22 @@
       return
     }
 
-    analyser && analyser.getFloatTimeDomainData(audioBuffer)
+    audioAnalyser && audioAnalyser.getFloatTimeDomainData(audioBuffer)
     var ac = autoCorrelate(audioBuffer, audioContext.sampleRate)
 
-    options.y.mx = Math.log(ac*ac)
-    options.x.mx = ac/100
-    console.log(options.y.mx)
-
-    var pos = {
-            x: options.moving
-                ? Math[options.x.fn](counter += options.flicker) * options.x.mx
-                : 0
-          , y: options.moving
-                ? Math[options.y.fn](counter += options.flicker) * options.y.mx
-                : 0
-        }
-      , spacing = options.randomSpacing
-                    ? options.elGridSpacing * Math.random() * ac
-                    : options.elGridSpacing
-      , i
-      , j
-    console.log(pos)
-    canvas.clear('#000')
+    canvas.clear('#fff')
     buffer.clear()
 
-    buffer.fillStyle('#fff')
-    for (i = Math.round(options.elGridY / 2) - options.elGridY;
-              i < Math.round(options.elGridY / 2); i++) {
-      for (j = Math.round(options.elGridX / 2) - options.elGridX;
-                j < Math.round(options.elGridY / 2); j++) {
-        buffer.fillRect(
-            pos.x - 20 + canvas.canvas.width / 2 - spacing * j
-          , pos.y - 20 + canvas.canvas.height / 2 - spacing * i
-          , 40
-          , 40
-        )
-      }
-    }
+    // draw flies
+    for (var i = 0; i < flies.length; i++) {
+      flies[i].move()
+      flies[i].draw(buffer)
+      flies[i].calculateZMod(ac/100)
+      // flies[i].speed =  //Math.log(ac*ac)
 
+      if (flies[i].checkTarget()) flies[i].setTarget()
+    }
+    canvas.drawImage(bgImage, 0, canvas.height/2 - bgImage.height/2 - 280, canvas.width, bgImage.width)
     canvas.drawImage(buffer.canvas, 0, 0
       , canvas.canvas.width
       , canvas.canvas.height
@@ -140,6 +251,10 @@
 
     window.requestAnimationFrame(step)
   }
+///////////
+
+
+
 
   function autoCorrelate( buf, sampleRate ) {
     var SIZE = buf.length
@@ -193,4 +308,4 @@
 
   document.addEventListener('DOMContentLoaded', run)
   document.addEventListener('mousemove', mouseMove)
-})()
+// })()
